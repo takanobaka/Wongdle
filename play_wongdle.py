4a046441219242bc77574ea9f4bc7ed424a734f5
bobbibrowny <wongtakashi@gmail.com>
2023-12-15 23:11:26 +0800

    added working worlde without interface with color

--- play_wongdle.py
+++ play_wongdle.py
@@ -1,9 +1,24 @@
 from wongdle import Wongdle
-
+from colorama import Fore
 
 def main():
     print("Hello World")
-    wongdle = Wongdle()
+    wongdle = Wongdle("APPLE")
+
+    while wongdle.can_attempt:
+        x = input("Type your guess: ")
+        if len(x)!=wongdle.WORD_LENGTH:
+            print(Fore.RED+f"Please input {wongdle.WORD_LENGTH} word: "+Fore.RESET) #format the print statemetn with dynamic variables!
+            continue
+        wongdle.attempt(x)
+        result = wongdle.guess(x)
+        print(*result, sep = "\n") #print the *result will print result[0], result[1], etc. etc.
+    if wongdle.is_solved:
+        print("Nice.")
+    else:
+        print("u dum")
+
+
 
 if __name__ == "__main__":
     main()
--- wongdle.py
+++ wongdle.py
@@ -1,9 +1,39 @@
+from letter_state import LetterState
+
 class Wongdle:
 
     MAX_ATTEMPT = 6
     WORD_LENGTH = 5
-    
+
     def __init__(self, secret: str):
         self.secret: str = secret
         self.attempts = []
         pass
+
+    def attempt(self,word:str):
+        self.attempts.append(word)
+    def guess(self,word:str):
+        result = []
+
+        for i in range(self.WORD_LENGTH):
+            character = word[i]
+            letter = LetterState(character)
+            letter.is_in_word = character in self.secret
+            letter.is_in_position = character == self.secret[i]
+            result.append(letter)
+        return result
+
+    @property
+    def is_solved(self):
+        return len(self.attempts)>0 and self.attempts[-1]==self.secret
+    
+    @property
+    def remaining_attempts(self) -> int:
+        return self.MAX_ATTEMPT - len(self.attempts)
+        
+    #property means you can use as attribute instead of a method (no need brackets)
+    @property
+    def can_attempt(self):
+        return self.remaining_attempts > 0 and not self.is_solved
+    
+    
