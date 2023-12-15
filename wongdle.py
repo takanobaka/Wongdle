43a0baefb1e15f612c6c035f9bdcc2d29c868d26
bobbibrowny <wongtakashi@gmail.com>
2023-12-15 22:05:43 +0800

    new changes

--- wongdle.py
+++ wongdle.py
@@ -1,3 +1,9 @@
 class Wongdle:
-    def __init__(self):
+
+    MAX_ATTEMPT = 6
+    WORD_LENGTH = 5
+    
+    def __init__(self, secret: str):
+        self.secret: str = secret
+        self.attempts = []
         pass
